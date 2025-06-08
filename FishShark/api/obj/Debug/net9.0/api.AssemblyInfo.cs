c3c2c52c9e19933c092930381dcddda2b295504d
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-06-08 17:25:39 +0800

    miscfiles

--- FishShark/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ FishShark/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8d9d01612f2938736d4c30d199a6a4ba68105f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4759bb25b980823fac8683d0ed9aacf5227f84e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
