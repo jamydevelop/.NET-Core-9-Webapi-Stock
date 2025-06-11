545502d72feb66d7151f2141c00c3dbeaddf3011
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-06-11 20:00:05 +0800

    misc files

--- FishShark/api/Program.cs
+++ FishShark/api/Program.cs
@@ -51,7 +51,7 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
     });
-    // builder.Services.AddOpenApi();
+
 
     builder.Services.AddDbContext<ApplicationDBContext>(options =>
     {
